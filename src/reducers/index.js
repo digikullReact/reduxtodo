ea96ca545452e5a7da2055ae33f11400698ea102
shubham dixit <shubhamdixit863@gmail.com>
2021-10-10 19:56:26 +0530

    adddd ui

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,27 +4,19 @@ const initialState = {
   value: 0,
 }
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const taskSlice = createSlice({
+  name: 'tasks',
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
+
+    addTask:(state,action)=>{
+
+    }
+    
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const {  } = taskSlice.actions
 
-export default counterSlice.reducer
+export default taskSlice.reducer
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterSlice from "../reducers"
+import taskSlice from "../reducers"
 
 export const store = configureStore({
   reducer: {
-      counter:counterSlice
+      tasks:taskSlice
   },
 })
